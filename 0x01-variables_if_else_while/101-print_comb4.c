f10206691e68bfddf4d712e98470f740f3859d59
MichaelAuditore <michael_v613@hotmail.com>
2019-09-19 23:40:33 +0000

    [Add] This C file works to print all the numbers that are not repeated digits in your 3 digits

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int a;
+	int b;
+	int c;
+
+	for (a = 48; a <= 57; a++)
+	{
+		for (b = 48 ; b <= 57 ; b++)
+		{
+			for(c = 48 ; c <= 57 ; c++)
+			{
+				if ((a != b) && (b != c) && (a != c) && (a < c) && (b < c) && (a < b))
+				{
+					putchar(a);
+					putchar(b);
+					putchar(c);
+					if ((a == 55) && (b == 56) && (c == 57))
+					{
+					}
+					else
+					{
+						putchar(',');
+						putchar(' ');
+					}
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
