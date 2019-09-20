5ff74d16258287ca24013bbe6145289b3c7a17a2
MichaelAuditore <michael_v613@hotmail.com>
2019-09-19 22:51:30 -0500

    Update 102-print_comb5.c

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -14,14 +14,14 @@ int main(void)
         {
                 for (b = 1 ; b < 100 ; b++)
                 {
-                        if((a % 10 + '0') != (b % 10 + '0'))
+                        if ((a % 10 + '0') != (b % 10 + '0'))
                         {
                                 putchar(a / 10 + '0');
                                 putchar(a % 10 + '0');
                                 putchar(' ');
                                 putchar(b / 10 + '0');
                                 putchar(b % 10 + '0');
-                                if ((a == 98) && (b ==99))
+                                if ((a == 98) && (b == 99))
                                 {
                                 }
                                 else
