be8ae2b4e1dfe42cf692b9a5a70214f47c6bef11
MichaelAuditore <michael_v613@hotmail.com>
2019-09-24 22:22:47 +0000

    [Add] Solved First Advanced task

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,48 @@
+#include "holberton.h"
+
+/**
+* print_times_table - initialize the program
+* This function prints the times table
+* @n: first integer
+*
+*/
+void print_times_table(int n)
+{
+	int a = 0;
+	int b;
+	int c;
+
+	if (n > 0 && n <= 15)
+	{
+		n++;
+		while (a < n)
+		{
+			_putchar('0');
+			b = 1;
+			while (b < n)
+			{
+				c = a * b++;
+				_putchar(',');
+				_putchar(' ');
+				if (c < 10)
+				{
+					_putchar(' ');
+					_putchar(c + '0');
+				}
+				else if (c >= 10 && c < 100)
+				{
+					_putchar(c / 10 + '0');
+					_putchar(c % 10 + '0');
+				}
+				else
+				{
+					_putchar(c / 100 + '0');
+					_putchar(c / 10 % 10 + '0');
+					_putchar(c % 10 + '0');
+				}
+			}
+			_putchar('\n');
+			a++;
+		}
+	}
+}
