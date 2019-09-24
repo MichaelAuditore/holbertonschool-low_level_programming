37c97d9c5515b7fed309167ea4c87f446ce824de
MichaelAuditore <michael_v613@hotmail.com>
2019-09-24 11:47:48 +0000

    [Fix] Previously the file don't contains the header file

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -3,14 +3,13 @@
 /**
  * main - start program
  * this function display the alphabet in lowercase
- * Return: Always 0 (Success)
+ * Return: Nothing (Success)
  */
-int main(void)
+void print_alphabet(void)
 {
 	int a;
 
 	for (a = 'a' ; a <= 'z' ; a++)
 		_putchar(a);
 	_putchar('\n');
-	return (0);
 }
