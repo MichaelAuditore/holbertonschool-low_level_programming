14ecee94ceb91a18e268bc189650461262876247
MichaelAuditore <michael_v613@hotmail.com>
2019-09-24 11:45:30 +0000

    [Fix] Previously the file don't contains the header file

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,4 +1,4 @@
-#include <stdio.h>
+#include "holberton.h"
 
 /**
  * main - start program
@@ -10,7 +10,7 @@ int main(void)
 	int a;
 
 	for (a = 'a' ; a <= 'z' ; a++)
-		putchar(a);
-	putchar('\n');
+		_putchar(a);
+	_putchar('\n');
 	return (0);
 }
