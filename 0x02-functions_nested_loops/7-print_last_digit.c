43204594598cf1b88733fc41cc2249409261157d
MichaelAuditore <michael_v613@hotmail.com>
2019-09-24 12:28:14 +0000

    [Fix] Solved fix in operation

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -2,15 +2,15 @@
 /**
  * _abs - initilize the program
  * This function will print the absolute value of a number
- * @c: is the char to evaluate
+ * @a: is the char to evaluate
  *
  * Return: absolute value
  */
-int _abs(int c)
+int _abs(int a)
 {
 	int sum = 0;
 
-	if (c < 0)
+	if (c <= 0)
 		sum = (c - c) + (-c);
 	else
 		sum = c;
