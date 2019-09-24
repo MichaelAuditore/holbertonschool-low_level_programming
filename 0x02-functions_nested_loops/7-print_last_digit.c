ed39e39070ab66f19effe8e56eb3e3c82085da64
MichaelAuditore <michael_v613@hotmail.com>
2019-09-24 15:37:32 +0000

    [Fix] Trying again

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,24 +1,5 @@
 #include "holberton.h"
 /**
- * _abs - initilize the program
- * This function will print the absolute value of a number
- * @a: is the char to evaluate
- *
- * Return: absolute value
- */
-int _abs(int a)
-{
-	int sum = 0;
-
-	if (a <= 0)
-		sum = a - (a * 2);
-	else
-		sum = a;
-	return (sum);
-}
-
-
-/**
  * print_last_digit - initialize the program
  * This function prints the last digit of a number
  * @c: this is the char to evaluate
@@ -27,10 +8,9 @@ int _abs(int a)
  */
 int print_last_digit(int c)
 {
-	int a;
-
-	a = _abs(c);
-	a %= 10;
-	_putchar(a + '0');
-	return (a);
+	if (c < 0)
+		c = c * -1;
+	c %= 10;
+	_putchar('0' + c);
+	return (c);
 }
