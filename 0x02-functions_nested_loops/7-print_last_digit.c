38563a48c2ff6995ad10b254b2c6d9f2ced49828
MichaelAuditore <michael_v613@hotmail.com>
2019-09-24 16:25:02 +0000

    [Fix] change the order of operations

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -2,18 +2,18 @@
 
 /**
  * print_last_digit - initialize the program
+ *
  * This function prints the last digit of a number
+ *
  * @c: this is the char to evaluate
  *
- * Return: last digit
+ * Return: @c last digit
  */
 int print_last_digit(int c)
 {
+	c %= 10;
 	if (c < 0)
-	{
 		c = c * -1;
-	}
-	c = c % 10;
 	_putchar('0' + c);
 	return (c);
 }
