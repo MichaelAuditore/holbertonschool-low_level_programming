5e237c4670fcf0fb3ed2cbb1e06f4597245396f9
MichaelAuditore <michael_v613@hotmail.com>
2019-09-25 22:15:53 +0000

    [Fix] Output count words solved

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -7,19 +7,20 @@
  */
 int main(void)
 {
-	long int a = 0;
-	long int b = 1;
+	long int a = 1;
+	long int b = 2;
 	long int c = 0;
-	long int d = 0;
+	long int d = 3;
 
-	while (c <= 49)
+	printf("1, 2, ");
+	while (c < 47)
 	{
-		d = a + b;
 		printf("%lu", d);
-		if (c != 49)
+		if(c != 46)
 			printf(", ");
 		a = b;
 		b = d;
+		d = a + b;
 		c++;
 	}
 	printf("\n");
