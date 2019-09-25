e0b12dd52bca0069239eeb987a5b3a596e9973f9
MichaelAuditore <michael_v613@hotmail.com>
2019-09-25 00:41:01 +0000

    [Fix] This changes allows to view the sum of all multiples of 3 or 5

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -12,7 +12,10 @@ int main(void)
 	int b;
 	for (a = 0 ; a < 1024 ; a++)
 	{
-		if ((a % 3) || (a % 5))
-			b += a;
+		b = a;
+		if ((a % 3 == 0) || (a % 5 == 0))
+			b = b + a;
 	}
+	printf("%d\n", b);
+	return (0);
 }
