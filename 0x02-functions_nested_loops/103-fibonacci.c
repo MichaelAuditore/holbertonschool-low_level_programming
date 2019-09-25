0bfc5e04d3e44b852a2c532064c40218db6242d4
MichaelAuditore <michael_v613@hotmail.com>
2019-09-25 23:31:00 +0000

    [Add] Less for finish

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - start the program
+ * This function prints the sum of all even numbers of fibonacci
+ * until 4,000.000
+ * Return: Always 0
+ */
+int main(void)
+{
+	int a = 0;
+	int b = 1;
+	int c = 0;
+}
