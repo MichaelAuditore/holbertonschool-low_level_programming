24b114a28fd8685c25606eb1a4705006ad679184
MichaelAuditore <michael_v613@hotmail.com>
2019-09-25 20:30:44 +0000

    [Failed Pull]

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -8,11 +8,15 @@
  */
 int main(void)
 {
-	int a;
-	int b;
-	for (a = 0 ; a < 1024 ; a++)
+	int a = 0;
+	int b = 0;
+
+	while (a < 1024)
 	{
-		if ((a % 3) || (a % 5))
-			b += a;
+		if (a % 3 == 0 || a % 5 == 0)
+			b = b + a;
+		a++;
 	}
+	printf("%d\n", b);
+	return (0);
 }
