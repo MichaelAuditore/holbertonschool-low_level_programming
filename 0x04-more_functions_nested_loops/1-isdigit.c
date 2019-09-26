824699634d5700ea0e57c80e5a8ed95477f4ec87
MichaelAuditore <michael_v613@hotmail.com>
2019-09-26 19:44:23 +0000

    [Add] First Solved file

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+
+/**
+ * _isdigit - start the program
+ * @c: Integer parameter
+ * Return: 1 is upper 0 is lower
+ */
+int _isdigit(int c)
+{
+	int a;
+
+	a = ((c >= 97 && c <= 122) ? 0 : 1);
+	return (a);
+}
