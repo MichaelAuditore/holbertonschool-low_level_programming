5169d05adce6678128e600f85bea734699e91641
MichaelAuditore <michael_v613@hotmail.com>
2019-09-26 20:00:23 +0000

    [Add] Second Solved

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,12 @@
+#include "holberton.h"
+
+/**
+ * mul - prints a multiply of two numbers
+ * @a : First integer
+ * @b : Second integer
+ * Return: Result of multiply two numbers
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
