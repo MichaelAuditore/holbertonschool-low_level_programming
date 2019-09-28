c7bdb7f848d710cbb4f1a23cd138bbfa361396c7
MichaelAuditore <michael_v613@hotmail.com>
2019-09-28 00:42:17 +0000

    [Add] Function prints an integer value

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,39 +1,18 @@
 #include "holberton.h"
 
 /**
- * print_number - print an integer number
- * @n: integer number to evaluate
- *
+ * print_number -  comes to print an integer
+ * @n: Integer value
  * not return
  */
 void print_number(int n)
 {
-	unsigned int number;
-	unsigned int reverse;
-	int lastDigit;
-
 	if (n < 0)
 	{
-		n = (n * -1);
 		_putchar('-');
+		n = n * -1;
 	}
-	if (n == 0)
-	{
-		_putchar('0');
-	}
-	number = n;
-	reverse = 0;
-	lastDigit = 0;
-	while (number > 0)
-	{
-		lastDigit = number % 10;
-		reverse = (reverse * 10) + lastDigit;
-		number = number / 10;
-	}
-	while (reverse > 0)
-	{
-		lastDigit = reverse % 10;
-		_putchar(lastDigit + '0');
-		reverse = reverse / 10;
-	}
+	if (n / 10 != 0)
+		print_number(n / 10);
+	_putchar('0' + n % 10);
 }
