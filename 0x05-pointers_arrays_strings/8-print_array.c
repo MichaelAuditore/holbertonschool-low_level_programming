308d1886f06ba39307e4ab266a486d1ccc3d9152
MichaelAuditore <michael_v613@hotmail.com>
2019-10-01 20:36:46 +0000

    [Fix] Evaluating when is 0

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -11,11 +11,9 @@ void print_array(int *a, int n)
 {
 	int i;
 
+	(n <= 0 ? printf("\n") : 1);
 	for (i = 0; i < n; i++)
 	{
-		if (i == (n - 1))
-			printf("%d\n", a[i]);
-		else
-			printf("%d, ", a[i]);
+		(i == (n - 1)) ? printf("%d\n", a[i]) : printf("%d, ", a[i]);
 	}
 }
