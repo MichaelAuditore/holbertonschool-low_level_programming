11c5b171b318dae68fb627a4ebef939b6e9eafc9
MichaelAuditore <michael_v613@hotmail.com>
2019-10-01 19:27:31 +0000

    [Add] prints the half of a string

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+
+/**
+ * puts_half - will print every other character of the string
+ * @str: Is the array to evaluate
+ * not return
+ */
+void puts_half(char *str)
+{
+	int i = 0;
+	int j;
+
+	while (i[str] != '\0')
+		i++;
+	(i % 2 == 0 ? (j = i / 2) : (j = ((i - 1) / 2)));
+	while (j < i)
+	{
+		_putchar(j[str]);
+		j++;
+	}
+	_putchar('\n');
+}
