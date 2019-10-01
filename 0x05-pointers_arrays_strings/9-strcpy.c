ac4107b1bcd61d32be13371aef91daacc877138f
MichaelAuditore <michael_v613@hotmail.com>
2019-10-01 21:01:13 +0000

    [Add] Copy string from an array to another array

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+
+/**
+ * _strcpy - store a copy of a pointer into another pointer
+ * @dest: Pointer one
+ * @src: Pointer two
+ * Return: Pointer
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int i = 0;
+
+	while (i[src] != '\0')
+	{
+		i[dest] = i[src];
+		i++;
+	}
+	i[dest] = '\0';
+	return (dest);
+}
