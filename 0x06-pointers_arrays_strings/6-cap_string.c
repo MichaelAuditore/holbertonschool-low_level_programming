051daef9648bf1989f57d34a3d590f0c3a39a67d
MichaelAuditore <michael_v613@hotmail.com>
2019-10-03 23:19:03 +0000

    [Add] Capitalize

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,30 @@
+#include "holberton.h"
+/**
+ * cap_string - convert to Capitalize all words
+ * @c: Pointer to evaluate
+ * Return: a String
+ */
+char *cap_string(char *c)
+{
+	int i;
+	int j;
+	int signals[14] = {',', ';', '.', '!', '?', '"', '(', ')', '{', '}',
+			   '\t', ' ', '\n', '\0'};
+
+	for (i = 0; c[i] != '\0'; i++)
+	{
+		if (c[i] >= 97 && c[i] <= 122)
+		{
+			j = 0;
+			for (; signals[j]; j++)
+			{
+				if (c[i - 1] == signals[j])
+				{
+					c[i] = c[i] - 32;
+					break;
+				}
+			}
+		}
+	}
+	return (c);
+}
