fc5a614667864da0caae97ffb952d264b49a3b23
MichaelAuditore <michael_v613@hotmail.com>
2019-10-07 18:51:07 +0000

    [Add] Strchar

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -8,34 +8,15 @@
  */
 char *_strchr(char *s, char c)
 {
-	int pos = 0;
-	int length = 0;
-	int f = 0;
-	int fin = 0;
+	int i;
 
-	for (; s[pos] != '\0'; pos++)
+	for (; *s; s++)
 	{
-		if (s[pos] == c)
+		if (*s == c)
 		{
-			f = 1;
 			break;
 		}
+		i--;
 	}
-	while (s[length] != '\0')
-		length++;
-	while (pos <= length - 1)
-	{
-		s[fin] = s[pos];
-		pos++;
-		fin++;
-	}
-	while (fin <= length -1)
-	{
-		s[fin] = '\0';
-		fin++;
-	}
-	if (f == 1)
-		return (s);
-	else
-		return (0);
+	return (*s == c ? s : 0);
 }
