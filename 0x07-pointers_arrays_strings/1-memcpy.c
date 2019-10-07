fb2de2140232fc898c6db2e163db5a39310791b0
MichaelAuditore <michael_v613@hotmail.com>
2019-10-07 17:21:17 +0000

    [Fix] Adding a condition when n bytes is null

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -11,10 +11,9 @@ char *_memcpy(char *dest, char *src, unsigned int n)
 {
 	unsigned int i;
 
-	for (i = 0; i < n && src[i] != '\0'; i++)
+	for (i = 0; i <= n - 1 && src[i] != '\0'; i++)
 	{
 		dest[i] = src[i];
 	}
-	dest[i] = src[i];
 	return (dest);
 }
