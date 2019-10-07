fb542dfbca315110fb39ca3a7a3122f0c5a92bc8
MichaelAuditore <michael_v613@hotmail.com>
2019-10-07 19:52:28 +0000

    [Add] Strspn

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,27 @@
+#include "holberton.h"
+
+/**
+ * _strspn - gets the length of a prefix substring
+ * @s: First String
+ * @accept: Second String
+ * Return: number of chars founded in accept
+ */
+unsigned int _strspn(char *s, char *accept)
+{
+	int count = 0;
+	int i;
+	int j;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (j = 0; accept[j] != '\0'; j++)
+		{
+			if (s[i] == accept[j])
+			{
+				count++;
+				s++;
+			}
+		}
+	}
+	return (count);
+}
