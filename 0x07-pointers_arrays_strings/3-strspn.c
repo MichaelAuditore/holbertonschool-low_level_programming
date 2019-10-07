ea7d2fd8adda7b51fe9b49406dc00f104ba4c771
MichaelAuditore <michael_v613@hotmail.com>
2019-10-07 21:03:46 +0000

    [Fix] Fixed when the last condition don't applies

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -9,22 +9,22 @@
 unsigned int _strspn(char *s, char *accept)
 {
 	int count = 0;
-	int i;
+	int i = 0;
 	int j;
 
-	for (i = 0; s[i] != '\0'; i++)
+	for (; s[i] != '\0'; i++)
 	{
 		for (j = 0; accept[j] != '\0'; j++)
 		{
 			if (s[i] == accept[j])
 			{
 				count++;
-				if (s[i + 1] != accept[j])
-				{
-					break;
-				}
-				s++;
+				break;
 			}
+		}
+		if (!accept[j])
+		{
+			break;
 		}
 	}
 	return (count);
