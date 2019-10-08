f4a23695ae98fb27c6d53bd94bb8b30a56415efd
MichaelAuditore <michael_v613@hotmail.com>
2019-10-08 00:58:29 +0000

    [Add] Do a diagonal adds

---
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,33 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * print_diagsums - generates the sums of the diagonal values of a matrix
+ *
+ * @a: matrix to get diagonals of
+ * @size: size of matrix
+ * Return: void
+ */
+void print_diagsums(int *a, int size)
+{
+	int add = 0;
+	int add2 = 0;
+	int i = 0;
+	int max = 0;
+
+	max = size * size;
+
+	while (i < max)
+	{
+		sum += a[i];
+		i = i + size + 1;
+	}
+	i = size - 1;
+	while (i < (max - 1))
+	{
+		sum2 += a[i];
+		i = i + (size - 1);
+	}
+
+	printf("%d, %d\n", sum, sum2);
+}
