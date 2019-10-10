4a3a88462a4fc4a3466e0a5f454b67ae32034010
MichaelAuditore <michael_v613@hotmail.com>
2019-10-10 02:15:31 +0000

    [Add] New headers and exercise 2

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+
+/**
+ * _print_rev_recursion - prints a string in reverse recursively
+ * @s: Starter address to print
+ * not return value
+ */
+void _print_rev_recursion(char *s)
+{
+	if (*s > 0)
+		_print_rev_recursion(s + 1);
+	_putchar(*s);
+	if (*s < 0)
+		_putchar('\n');
+}
