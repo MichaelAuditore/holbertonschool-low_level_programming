ea1a4d8e736036b3851b977e077ae4f4d7ba742c
MichaelAuditore <michael_v613@hotmail.com>
2019-10-13 17:48:39 +0000

    [Add] Change money to cents

---
+++ 0x0A-argc_argv/100-change.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - change an amount of money to cents
+ * @argc: arguments counter
+ * @argvc: array of arguments
+ * Return: Always 0
+ */
+int main(int argc, char **argvc)
+{
+	int i;
+	int cents = 0;
+	int money = atoi(argvc[argc - 1]);
+	int cent[5] = {25, 10, 5, 2, 1};
+
+	if (argc != 2)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	if (money < 0)
+	{
+		printf("0\n");
+		return (0);
+	}
+	while (money > 0)
+	{
+		i = 0;
+		while (i < 5)
+		{
+			if (money % cent[i] == 0)
+			{
+				money -= cent[i];
+				cents++;
+				break;
+			}
+			i++;
+		}
+	}
+	printf("%d\n", cents);
+	return (0);
+}
