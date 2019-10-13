129b89ec4fc8e828f1f201585cd9f70f782e8f0f
MichaelAuditore <michael_v613@hotmail.com>
2019-10-13 18:41:52 +0000

    [Fix] Output fixed

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -4,38 +4,42 @@
 /**
  * main - change an amount of money to cents
  * @argc: arguments counter
- * @argvc: array of arguments
+ * @argv: array of arguments
  * Return: Always 0
  */
-int main(int argc, char **argvc)
+int main(int argc, char **argv)
 {
-	int i;
-	int cents = 0;
-	unsigned int money = atoi(argvc[argc - 1]);
-	int cent[5] = {25, 10, 5, 2, 1};
+	int ncoins = 0;
+	int cents = atoi(argv[argc - 1]);
 
 	if (argc != 2)
 	{
 		printf("Error\n");
 		return (1);
 	}
-	if (atoi(argvc[argc - 1]) < 0)
+	if (cents <= 0)
 	{
 		printf("0\n");
 		return (0);
 	}
-	while (money > 0)
+	while (cents > 0)
 	{
-		while (i < 5)
-		{
-			if (money % cent[i] == 0)
-			{
-				money -= cent[i];
-				cents++;
-			}
-			money % cent[i] != 0 ? i++ : i;
-		}
+		ncoins += cents / 25;
+		cents %= 25;
+
+		ncoins += cents / 10;
+		cents %= 10;
+
+		ncoins += cents / 5;
+		cents %= 5;
+
+		ncoins += cents / 2;
+		cents %= 2;
+
+		ncoins += cents;
+		cents %= 1;
+
 	}
-	printf("%d\n", cents);
+	printf("%d\n", ncoins);
 	return (0);
 }
