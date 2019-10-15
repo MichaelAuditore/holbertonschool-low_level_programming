e2396b57ea5dfa63bb14762ae463b1883fa400a7
MichaelAuditore <michael_v613@hotmail.com>
2019-10-15 18:17:31 +0000

    [Fix] Don't it use global variables

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -1,6 +1,44 @@
 #include "holberton.h"
 #include <stdlib.h>
 /**
+ * _strlen - returns the length of a given string
+ * @s: string to check the length of
+ * Return: returns the length of the string
+ */
+int _strlen(char *s)
+{
+	int i;
+
+	i = 0;
+	while (s[i] != '\0')
+		i++;
+	return (i);
+}
+/**
+ * _strcpy - copies an entire string to a buffer
+ * @src: source string to copy
+ * @dest: buffer to copy string into
+ * Return: returns pointer to copied string
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int i;
+	char *tmp;
+
+	i = 0;
+	while (src[i] != '\0')
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	dest[i] = src[i];
+
+	tmp = dest;
+
+	return (tmp);
+}
+
+/**
  * str_concat - concatenate two strings in a new pointer
  * @s1: Address of string one
  * @s2: Address of string two
@@ -9,29 +47,23 @@
 char *str_concat(char *s1, char *s2)
 {
 	char *ar;
-	int fs = 0;
-	int ls = 0;
 	int i = 0;
-	int sl = 0;
+	int j = 0;
 
-	while (s1[fs])
-		fs++;
-	while (s2[ls])
-		ls++;
-
-	ar = (char *) malloc(((fs + ls) * sizeof(char)) + 1);
+	ar = malloc(_strlen(s1) + _strlen(s2) + 1);
 	if (ar == NULL)
 		return (NULL);
-	while (i < fs)
+	while (i < (_strlen(s1) + _strlen(s2)))
 	{
-		ar[i] = s1[i];
+		if (s1[i] != '\0')
+			ar[i] = s1[i];
+		if (i >= _strlen(s1) && s2[j] != '\0')
+		{
+			ar[i] = s2[j];
+			j++;
+		}
 		i++;
 	}
-	while (i < (fs + ls))
-	{
-		ar[i] = s2[sl];
-		i++;
-		sl++;
-	}
+	ar[i] = '\0';
 	return (ar);
 }
