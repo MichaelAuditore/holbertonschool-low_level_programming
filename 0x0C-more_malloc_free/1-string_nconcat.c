9dc355cff2832fd739afa65215d6a9eb93a154ae
MichaelAuditore <michael_v613@hotmail.com>
2019-10-17 20:54:07 +0000

    [Fix] Valgrind don't show errors

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,36 +1,6 @@
 #include "holberton.h"
 #include <stdlib.h>
 /**
- * _strlen - Returns the length of a string
- * @s: Char array
- * Return: length of a string
- */
-unsigned int _strlen(char *s)
-{
-	return ((!*s) ? 0 : 1 + _strlen(s + 1));
-}
-/**
- * _strncat - concatenate an index from a string in another string
- * @dest: First string
- * @src: Second String
- * @n: index
- * Return: A string
- */
-char *_strncat(char *dest, char *src, unsigned int n)
-{
-	int ld = _strlen(dest);
-	unsigned int i = 0;
-
-	while (i < n && src[i] != '\0')
-	{
-		dest[ld + i] = src[i];
-		i++;
-	}
-	dest[ld + i] = '\0';
-	return (dest);
-}
-
-/**
  * string_nconcat - concatenate a n bytes of a string into another
  * @s1: First Address of string
  * @s2: Second Address of string
@@ -40,24 +10,36 @@ char *_strncat(char *dest, char *src, unsigned int n)
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 	char *str;
-	char *st1 = s1;
-	char *st2 = s2;
-	int i = 0;
+	unsigned int i = 0;
+	unsigned int len1 = 0;
+	unsigned int len2 = 0;
+	unsigned int j = 0;
 
-	if (st1 == NULL)
-		st1 = "";
-	if (st2 == NULL)
-		st2 = "";
+	while (s1[len1])
+		len1++;
+	while (s2[len2])
+		len2++;
 
-	(n >= _strlen(st2)) ? (n = _strlen(st2)) : 1;
-	str = malloc((_strlen(st1) + n + 1) * sizeof(char));
+	if (n >= len2)
+		n = len2;
+
+	str = malloc((len1 + n + 1) * sizeof(char));
 	if (str == NULL)
+	{
+		free(str);
 		return (NULL);
+	}
+	str[0] = 0;
 	while (s1[i] != '\0')
 	{
 		str[i] = s1[i];
 		i++;
 	}
-	_strncat(str, st2, n);
+	while (j < n && s2[j] != '\0')
+	{
+		str[len1 + j] = s2[j];
+		j++;
+	}
+	str[len1 + j] = '\0';
 	return (str);
 }
