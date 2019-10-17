b9ec16942dc9792bd3ed3ceae044ff8430dc1cab
MichaelAuditore <michael_v613@hotmail.com>
2019-10-17 21:07:13 +0000

    [Fix] Valgrind don't show errors

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -15,13 +15,16 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	unsigned int len2 = 0;
 	unsigned int j = 0;
 
-	while (s1[len1])
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
+
+	while (s1[len1] != '\0')
 		len1++;
-	while (s2[len2])
+	while (s2[len2] != '\0')
 		len2++;
 
-	s1 = (!s1) ? "" : s1;
-	s2 = (!s2) ? "" : s2;
 	if (n >= len2)
 		n = len2;
 
