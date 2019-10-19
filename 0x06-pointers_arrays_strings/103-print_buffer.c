ff0e11b46f5839cadb4f94e1c129a1229400e804
MichaelAuditore <michael_v613@hotmail.com>
2019-10-19 17:51:38 +0000

    [Fix] Output fixed when the words are odd or not

--- 0x06-pointers_arrays_strings/103-print_buffer.c
+++ 0x06-pointers_arrays_strings/103-print_buffer.c
@@ -1,6 +1,73 @@
 #include "holberton.h"
 #include <stdio.h>
 /**
+ * print_chars - print 10 characters latest a new line
+ * @b: Address of string to print
+ * @pos: position to start to iterate
+ * @size: Size of string
+ * Return: The last position of iteration
+ */
+int print_chars(char *b, int pos, int size)
+{
+	int i = pos;
+
+	while (i < size)
+	{
+		if (b[i] >= 32 && b[i] <= 126)
+			printf("%c", b[i]);
+		else
+			printf("%c", '.');
+		i++;
+		if (i != 0 && i % 10 == 0)
+		{
+			printf("\n");
+			break;
+		}
+	}
+	return (i);
+}
+/**
+ * hexa_values - print two hexadecimal values
+ * @b: Address of string to print
+ * @pos: position to start to iterate
+ * @size: the size of string
+ * Return: The last position of iteration
+ */
+int hexa_values(char *b, int pos, int size)
+{
+	int i = pos;
+	int s = size;
+
+	while (s % 10 != 0)
+		s++;
+	while (i < size)
+	{
+		if (i % 10 == 0)
+			printf("%08x:", i);
+		if (i % 2 == 0)
+			printf(" ");
+		if (b[i] >= 32 && b[i] <= 126)
+			printf("%x", b[i]);
+		else
+			printf("0%x", b[i]);
+		if (i == size - 1 && size % 10 != 0)
+		{
+			while (i < s)
+			{
+				printf(" ");
+				printf(" ");
+				i++;
+			}
+			printf(" ");
+		}
+		i++;
+		if (i % 10 == 0 && i != 0)
+			break;
+	}
+	printf(" ");
+	return (i);
+}
+/**
  * print_buffer - prints a buffer of a string
  * @b: buffer to print
  * @size: size of the buffer
@@ -8,52 +75,20 @@
 void print_buffer(char *b, int size)
 {
 	int i = 0;
+	int li;
 	int j = 0;
-	int s = size;
+	int lp;
 
+	if (*b == '\0')
+		size = -1;
 	while (i < size)
 	{
-		if (i % 10 == 0)
-			printf("%08x:", i);
-		while (s % 10 != 0)
-			s++;
-		while (i < size)
-		{
-			if (i % 2 == 0)
-				printf(" ");
-			if (b[i] >= 32 && b[i] <= 126)
-				printf("%x", b[i]);
-			else
-				printf("0%x", b[i]);
-			if (i == size - 1 && size % 10 != 0)
-			{
-				while (i < s)
-				{
-					printf(" ");
-					printf(" ");
-					i++;
-				}
-			}
-			i++;
-			if (i % 10 == 0 && i != 0)
-			{
-				break;
-			}
-		}
-		printf(" ");
-		while (j < size)
-		{
-			if (b[j] >= 32 && b[j] <= 126)
-				printf("%c", b[j]);
-			else
-				printf("%c", '.');
-			j++;
-			if (j != 0 && j % 10 == 0)
-			{
-				printf("\n");
-				break;
-			}
-		}
+		li = i;
+		li = hexa_values(b, li, size);
+		i = li;
+		lp = j;
+		lp = print_chars(b, lp, size);
+		j = lp;
 	}
 	printf("\n");
 }
