18d44ccda50a53d09e48324a362996a4acbdd06d
MichaelAuditore <michael_v613@hotmail.com>
2019-10-19 23:19:03 +0000

    [Fix] Evaluating some main functions

--- 0x06-pointers_arrays_strings/103-print_buffer.c
+++ 0x06-pointers_arrays_strings/103-print_buffer.c
@@ -19,12 +19,13 @@ void print_spaces(int pos, int size, int old_size)
 	}
 	if (pos % 2 == 0 && old_size - pos == 1)
 	{
-		while (pos <= size)
+		while (pos < size)
 		{
 			printf(" ");
 			printf(" ");
 			pos++;
 		}
+		printf(" ");
 	}
 }
 /**
