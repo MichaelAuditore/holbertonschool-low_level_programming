c9d0f02667db47045dc41184345af9799a9ef5c0
MichaelAuditore <michael_v613@hotmail.com>
2019-10-21 16:31:31 +0000

    [Add] New directory with preprocessor

---
+++ 0x0D-preprocessor/101-preprocessor_abuse.c
@@ -0,0 +1,8 @@
+/**
+ * main - prints a String
+ * System call to write a String "Hello, Holberton"
+ */
+void main(void)
+{
+	write(0, "Hello, Holberton\n", 17);
+}
---
+++ 0x0D-preprocessor/README.md
@@ -0,0 +1,1 @@
+#Preprocesor Directives, how it works, and exercises
