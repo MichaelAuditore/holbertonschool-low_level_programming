d3a70051ed0902892db7c9cfb3d2a41ca3e120d0
MichaelAuditore <michael_v613@hotmail.com>
2019-10-21 18:50:19 +0000

    [Add] New directory about structures and typedef

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,15 @@
+#ifndef _DOG_H
+#define _DOG_H
+/**
+ * struct dog - this structure initializes 3 variables
+ * @name: First member
+ * @age: Second member
+ * @owner: Third member
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
