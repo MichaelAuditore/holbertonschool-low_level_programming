61fd0a37f772b788a3866c16346561bcfba4fc29
MichaelAuditore <michael_v613@hotmail.com>
2019-10-21 18:58:08 +0000

    [Add&Modify] New prototype new function

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,14 @@
+#include "dog.h"
+/**
+ * init_dog - initialize an instance for struct dog
+ * @d: My pointer to struct dog
+ * @name: Name of Dog
+ * @age: Age of Dog
+ * @owner: The ownership of Dog
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	(*d).name = name;
+	(*d).age = age;
+	(*d).owner = owner;
+}
