a0050427c2e7d875b66de24532eccafb38c0ac2e
MichaelAuditore <michael_v613@hotmail.com>
2019-10-24 01:30:34 +0000

    [Fix]: Initializing variables after conditionals to it don't generate Segmentation fault

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -10,11 +10,11 @@
  */
 int main(int argc, char **argv)
 {
-	int res;
-	int n1 = atoi(argv[1]);
-	int n2 = atoi(argv[3]);
+	int res = 0;
+	int n1;
+	int n2;
 
-	if (argc > 4 || argc < 4)
+	if (argc != 4)
 	{
 		printf("Error\n");
 		exit(98);
@@ -25,6 +25,8 @@ int main(int argc, char **argv)
 		printf("Error\n");
 		exit(99);
 	}
+	n1 =  atoi(argv[1]);
+	n2 =  atoi(argv[3]);
 	if (n2 == 0 && (*argv[2] == '/' || *argv[2] == '%'))
 	{
 		printf("Error\n");
