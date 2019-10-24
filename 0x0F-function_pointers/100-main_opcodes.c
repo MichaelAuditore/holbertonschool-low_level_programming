7efa9529628c89588f22f2ca4e34f15066702e48
MichaelAuditore <michael_v613@hotmail.com>
2019-10-24 02:36:59 +0000

    [Add] prints the opcodes of the current function

---
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+/**
+ * main - prints the opcodes of current function
+ * @argc: Counter of arguments
+ * @argv: Pointer to pointer
+ * Return: Always 0
+ */
+int main(int argc, char **argv)
+{
+	int i;
+	int bytes;
+	char *m;
+
+	if (argc != 2)
+	{
+		printf("Error\n");
+		exit(1);
+	}
+	bytes = atoi(argv[1]);
+	if (bytes < 0)
+	{
+		printf("Error\n");
+		exit(2);
+	}
+	m = (unsigned char *)main;
+	for (i = 0; i < bytes; i++)
+		printf("%x ", m[i]);
+	printf("%x\n", m[i]);
+}
