61e8e0310daae2463d8602a2322ab743a45c5a92
MichaelAuditore <michael_v613@hotmail.com>
2019-10-26 01:39:57 +0000

    [Feature]: New prototypes added to call functions, and new structure

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,7 +1,20 @@
 #ifndef VARIADIC_F_H
 #define VARIADIC_F_H
-int _putchar(char c);
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdarg.h>
+/**
+ * struct formats - struct of formats
+ * @c: Char to validate waht will I print
+ * @f: pointer to function to call the print
+ */
+typedef struct formats
+{
+	char c;
+	void (*f)();
+} form_at;
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
+void print_all(const char * const format, ...);
 #endif
