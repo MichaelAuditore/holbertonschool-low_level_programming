dce78122a5338efa9f1d75dfd6a06b01593cabf5
MichaelAuditore <michael_v613@hotmail.com>
2019-10-26 01:37:34 +0000

    [Feature]: Other way to print formats "%s, f, c, i"

---
+++ 0x10-variadic_functions/3-other_form_print_all.c
@@ -0,0 +1,42 @@
+#include "variadic_functions.h"
+/**
+ * print_all - prints argum
+ * @format: All formats to print
+ * @...: varargs
+ */
+void print_all(const char * const format, ...)
+{
+	va_list list;
+	int i = 0;
+	char *str;
+
+	va_start(list, format);
+	while (format[i] != '\0')
+	{
+		switch (format[i])
+		{
+		case 'i':
+			printf("%i", va_arg(list, int));
+			break;
+		case 'c':
+			printf("%c", (char) va_arg(list, int));
+			break;
+		case 'f':
+			printf("%f", (float) va_arg(list, double));
+			break;
+		case 's':
+			str = va_arg(list, char *);
+			if (str == NULL)
+				str = "(nil)";
+			printf("%s", str);
+			break;
+		}
+		if (format[i + 1] != '\0')
+		{
+			printf(", ");
+		}
+		i++;
+	}
+	printf("\n");
+	va_end(list);
+}
