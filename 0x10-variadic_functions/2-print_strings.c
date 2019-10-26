09c7427c35e8c3fffcc6b171119e39228f3e48e3
MichaelAuditore <michael_v613@hotmail.com>
2019-10-26 01:39:01 +0000

    [Update] At the beginning I was using my own putchar function to print

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -1,23 +1,7 @@
 #include "variadic_functions.h"
 #include <stdarg.h>
 #include <stdlib.h>
-/**
- * split - split each string
- * @i: position of iterator
- * @n: const of number of elements
- * @separator: It's the character to separate
- */
-void split(unsigned int i, const unsigned int n, const char *separator)
-{
-	if (i != n - 1)
-	{
-		if (separator != NULL)
-			_putchar(*separator);
-		_putchar(' ');
-	}
-	else
-		_putchar('\n');
-}
+#include <stdio.h>
 /**
 * print_strings - print all strings arguments
 * @separator: Represents the character to separate the strings
@@ -28,8 +12,6 @@ void print_strings(const char *separator, const unsigned int n, ...)
 {
 	va_list strings;
 	unsigned int i;
-	int j;
-	int ic;
 	char *str;
 
 	if (n == 0)
@@ -39,25 +21,14 @@ void print_strings(const char *separator, const unsigned int n, ...)
 
 	for (i = 0; i < n; i++)
 	{
-		j = 0;
 		str = va_arg(strings, char *);
 		if (str == NULL)
 			str = "(nil)";
-		while (str[j] != '\0')
-			j++;
-		if (j > 1)
-		{
-			for (ic = 0; ic < j; ic++)
-			{
-				_putchar(str[ic]);
-			}
-			split(i, n, separator);
-		}
+		printf("%s", str);
+		if (i != n - 1)
+			printf("%s ", separator);
 		else
-		{
-			_putchar(str[j]);
-			_putchar(' ');
-		}
+			printf("\n");
 	}
 	va_end(strings);
 }
