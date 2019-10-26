764a47caf7794ccdbb53ff30581b3a0194d33bb1
MichaelAuditore <michael_v613@hotmail.com>
2019-10-26 17:30:48 +0000

    [Fix] Prints coma when there is a parameter to print

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -36,10 +36,9 @@ void print_all(const char * const format, ...)
 			printf("%s", str);
 			break;
 		}
-		if (format[i + 1] != '\0')
-		{
+		if ((format[i] == 'c' || format[i] == 'i' || format[i] == 'f'
+		     || format[i] == 's') && format[i + 1] != '\0')
 			printf(", ");
-		}
 		i++;
 	}
 	printf("\n");
