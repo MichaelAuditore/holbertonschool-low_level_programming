ecb573beb3c5aaf8bcf3243ada08bb47fa9cb180
MichaelAuditore <michael_v613@hotmail.com>
2019-10-26 17:27:25 +0000

    [Fix] print (nil) when there isn't a parameter to print

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -11,23 +11,24 @@
 void print_strings(const char *separator, const unsigned int n, ...)
 {
 	va_list strings;
-	unsigned int i;
+	int i;
 	char *str;
 
-	if (n == 0)
+	if (n < 1)
 	{
 		printf("\n");
 		return;
 	}
 	va_start(strings, n);
 
-	for (i = 0; i < n; i++)
+	for (i = 0; i < (int) n; i++)
 	{
 		str = va_arg(strings, char *);
-		if (str == NULL)
+		if (str != NULL)
+			printf("%s", str);
+		else
 			printf("(nil)");
-		printf("%s", str);
-		if (i != n - 1 && separator != NULL)
+		if (i != (int) n - 1 && separator != NULL)
 			printf("%s", separator);
 	}
 	printf("\n");
