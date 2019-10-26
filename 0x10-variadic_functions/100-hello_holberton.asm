8a497e1816a9d87c50c70f4b097925597eafacaa
MichaelAuditore <michael_v613@hotmail.com>
2019-10-26 17:03:46 +0000

    [Fix]: change an instruction because didn't do the properly call

--- 0x10-variadic_functions/100-hello_holberton.asm
+++ 0x10-variadic_functions/100-hello_holberton.asm
@@ -1,13 +1,16 @@
-section .data
-	msg db "Hello, holberton", 10
-	section .miguel
 	global main
+
+	section .text
 main:
 	mov rax, 1
 	mov rdi, 1
 	mov rsi, msg
 	mov rdx, 17
 	syscall
-	mov rax, 60
-	mov rdi, 0
+
+	mov eax, 60
+	mov rdi, rdi
 	syscall
+
+msg:
+	db "Hello, Holberton", 10
