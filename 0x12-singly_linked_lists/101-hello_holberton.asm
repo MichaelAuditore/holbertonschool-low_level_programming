982cb4d9172876b11ef1ed0d307f1e3347d62915
MichaelAuditore <michael_v613@hotmail.com>
2019-10-31 22:06:03 +0000

    [Fix] Output fixed

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,18 +1,18 @@
 section .data
-	fmt     db "%s",17
+	fmt 	db "%s"
 	msg1    db "Hello, Holberton",10
 
-	    section .text
-	    extern printf
-	    global main
+	section .text
+	extern printf
+	global main
 
 main:
-	    mov  edx, fmt
-	    mov  esi, 0
-	    mov  edi, msg1
-	    mov  eax, 0     	; no f.p. args
-	    call printf
+	mov edx, fmt
+	mov esi, 0
+	mov  edi, msg1
+	mov  eax, 0
+	call printf
 
-	    mov  ebx, 0     	; return value
-	    mov  eax, 0
-	    int  0x80
+	mov  ebx, 1
+	mov  eax, 0
+	int  0x80
