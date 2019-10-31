3d3a6a6a3eac33d7cf80815a55161f3070ae137f
MichaelAuditore <michael_v613@hotmail.com>
2019-10-31 17:55:00 +0000

    [Fix] when no assigns memory dinamically return NULL

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -18,7 +18,8 @@ int _strlen(const char *str)
 list_t *add_node(list_t **head, const char *str)
 {
 	list_t *new = malloc(sizeof(list_t));
-
+	if (new == NULL)
+		return (NULL);
 	if (str != NULL)
 	{
 		new->str = strdup(str);
