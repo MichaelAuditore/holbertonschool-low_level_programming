944d69b7273a61cae9fb248ba7ef690461824019
MichaelAuditore <michael_v613@hotmail.com>
2019-11-04 22:27:16 +0000

    [Add] new file sum all elements of a list

---
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -0,0 +1,14 @@
+#include "lists.h"
+/**
+ * sum_listint - sum all elements of a list
+ * @head: Linked list
+ * Return the sum of all elements
+ */
+int sum_listint(listint_t *head)
+{
+	if (head != NULL)
+	{
+		return (head->n + sum_listint(head->next));
+	}
+	return (0);
+}
