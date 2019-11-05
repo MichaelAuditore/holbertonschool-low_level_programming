0634df62faf9830459a3dc70546de8bb714697a8
MichaelAuditore <michael_v613@hotmail.com>
2019-11-05 15:51:55 +0000

    [Fix] Length of the list

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -10,7 +10,6 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
 	listint_t *new;
 	listint_t *tmp = *head;
-	listint_t *tmp2 = *head;
 	int i = 0;
 
 	new = malloc(sizeof(listint_t));
@@ -18,30 +17,20 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	if (new == NULL)
 		return (NULL);
 
-	if (head == NULL)
+	if (head == NULL && idx > 0)
 		return (NULL);
 
-	while (tmp != NULL)
-	{
-		tmp = tmp->next;
-		i++;
-	}
-	if (i < (int) idx)
-	{
-		free(new);
-		return (NULL);
-	}
 	for (i = 0; i < (int) idx - 1; i++)
 	{
-		if (tmp2 == NULL && (int) idx - i > 0)
+		if (tmp == NULL && idx - i > 0)
 		{
 			free(new);
 			return (NULL);
 		}
-		tmp2 = tmp2->next;
+		tmp = tmp->next;
 	}
 	new->n = n;
-	new->next = tmp2->next;
-	tmp2->next = new;
+	new->next = tmp->next;
+	tmp->next = new;
 	return (new);
 }
