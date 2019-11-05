65cf6f1ad5cd3600ab4b0a80a6692e6573eca42a
MichaelAuditore <michael_v613@hotmail.com>
2019-11-05 14:27:09 +0000

    [Fix] Validating NULL without another variable

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -5,11 +5,10 @@
  */
 void free_listint2(listint_t **head)
 {
-	listint_t **tmp = head;
 	listint_t *current = *head;
 	listint_t *next;
 
-	if (tmp == NULL)
+	if (head == NULL)
 		return;
 	if (current == NULL)
 		return;
