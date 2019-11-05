4f91e0576d4e88481fb82b9d90e7be4484558482
MichaelAuditore <michael_v613@hotmail.com>
2019-11-05 14:00:18 +0000

    [Fix] Betty style

--- 0x13-more_singly_linked_lists/8-sum_listint.c
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -2,7 +2,7 @@
 /**
  * sum_listint - sum all elements of a list
  * @head: Linked list
- * Return the sum of all elements
+ * Return: the sum of all elements
  */
 int sum_listint(listint_t *head)
 {
