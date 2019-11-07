83416a4247ab7e8950a54779f8ee1c6042da54d5
MichaelAuditore <michael_v613@hotmail.com>
2019-11-07 21:16:14 +0000

    [Add] Get a bit at a given position

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+/**
+ * get_bit - get bit at a given index
+ * @n: Number to get the bit
+ * @index: index to get the bit
+ * Return: The bit at a given position
+ */
+int get_bit(unsigned long int n, unsigned int index)
+{
+	int bit = -1;
+
+	if (n > 0)
+	{
+		bit = (n >> index) & 1;
+	}
+	return (bit);
+}
