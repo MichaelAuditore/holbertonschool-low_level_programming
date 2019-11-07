592df55e7925fa0a43d1562ab4b7000635f3c61a
MichaelAuditore <michael_v613@hotmail.com>
2019-11-07 21:22:15 +0000

    [Fix] Validating when there is overflow

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -9,9 +9,9 @@ int get_bit(unsigned long int n, unsigned int index)
 {
 	int bit = 0;
 
+	if (index > (sizeof(n) * 8))
+		return (-1);
 	if (n > 0)
-	{
 		bit = (n >> index) & 1;
-	}
 	return (bit);
 }
