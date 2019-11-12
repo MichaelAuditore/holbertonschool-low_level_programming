79f7e04aa1d8d7a33959b2bf6c4ac29d2810914f
MichaelAuditore <michael_v613@hotmail.com>
2019-11-12 20:54:43 +0000

    [Fix] Validating when content is NULL

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -16,9 +16,14 @@ int create_file(const char *filename, char *text_content)
 		length++;
 	if (filename == NULL)
 		return (-1);
-	file = open(filename, O_CREAT | O_RDWR, 0600);
+	file = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
 	if (file == -1)
 		return (-1);
+	if (text_content == NULL)
+	{
+		close(file);
+		return (1);
+	}
 	w = write(file, text_content, length);
 	if (w == -1)
 	{
