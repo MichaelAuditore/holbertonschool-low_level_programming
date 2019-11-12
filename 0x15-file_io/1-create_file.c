508bd62f6b93430861d947ed6778f93dafd3aa41
MichaelAuditore <michael_v613@hotmail.com>
2019-11-12 21:07:17 +0000

    [Fix] Verifying close file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -12,8 +12,6 @@ int create_file(const char *filename, char *text_content)
 	int length = 0;
 	int w = 0;
 
-	while (text_content[length] != '\0')
-		length++;
 	if (filename == NULL)
 		return (-1);
 	file = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
@@ -21,8 +19,11 @@ int create_file(const char *filename, char *text_content)
 		return (-1);
 	if (text_content == NULL)
 		return (close(file) == -1 ? -1 : 1);
+	while (text_content[length] != '\0')
+		length++;
 	w = write(file, text_content, length);
 	if (w == -1)
 		return (-1);
-	return (close(file) == -1 ? -1 : 1);
+	close(file);
+	return (1);
 }
