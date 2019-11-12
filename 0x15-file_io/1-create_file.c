b41e7ca84e1fdb00067c833a090f66eb38a154d0
MichaelAuditore <michael_v613@hotmail.com>
2019-11-12 20:47:31 +0000

    [Add] Create file using syscalls

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,30 @@
+#include "holberton.h"
+#include <stdio.h>
+/**
+ * create_file - create file using syscalls
+ * @filename: Name of new file
+ * @text_content: Content of new file
+ * Return: 1 is success or -1 is not
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int file;
+	int length = 0;
+	int w = 0;
+
+	while (text_content[length] != '\0')
+		length++;
+	if (filename == NULL)
+		return (-1);
+	file = open(filename, O_CREAT | O_RDWR, 0600);
+	if (file == -1)
+		return (-1);
+	w = write(file, text_content, length);
+	if (w == -1)
+	{
+		close(file);
+		return (-1);
+	}
+	close(file);
+	return (1);
+}
