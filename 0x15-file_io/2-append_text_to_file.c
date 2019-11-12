5dddc56f432b1c906274b05bdcee49f5e84226c1
MichaelAuditore <michael_v613@hotmail.com>
2019-11-12 21:28:41 +0000

    [Feature] Function to concatenate a string into a file

---
+++ 0x15-file_io/2-append_text_to_file.c
@@ -0,0 +1,34 @@
+#include "holberton.h"
+#include <stdio.h>
+/**
+ * append_text_to_file - append text to a file using syscalls
+ * @filename: Reference to the name of the file
+ * @text_content: Reference to text will come into file
+ */
+int append_text_to_file(const char *filename, char *text_content)
+{
+	int file;
+	int len = 0;
+	int w = 0;
+
+	if (filename == NULL)
+		return (0);
+	file = open(filename, O_WRONLY | O_APPEND);
+
+	if (file == -1)
+		return (-1);
+
+	if (text_content == NULL && file == 1)
+	{
+		close(file);
+		return (1);
+	}
+	while (text_content[len] != '\0')
+		len++;
+
+	write (file, text_content, len);
+	if (w == -1)
+		return (-1);
+	close(file);
+	return (1);
+}
