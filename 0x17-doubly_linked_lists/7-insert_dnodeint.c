e36af2c9772bdfbfe0a0083aa0278612897a3bfa
MichaelAuditore <michael_v613@hotmail.com>
2019-12-14 18:33:01 +0000

    [fix] removing conditions

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -25,7 +25,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 
 	if (idx > dlistint_len(*h))
 		return (NULL);
-	if ((*h == NULL && idx > 0) || h == NULL)
+	if ((*h == NULL && idx > 0))
 		return (NULL);
 	if (idx == 0 || *h == NULL)
 		new = add_dnodeint(h, n);
