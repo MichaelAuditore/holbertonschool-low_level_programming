b20cb7e03fb28904676339e73449d7c157959299
MichaelAuditore <michael_v613@hotmail.com>
2019-12-14 18:29:22 +0000

    [Fix] Validating conditions by separate statements

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -23,7 +23,9 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	dlistint_t *tmp = *h;
 	unsigned int i = 0;
 
-	if (idx > dlistint_len(*h) || (*h == NULL && idx > 0) || h == NULL)
+	if (idx > dlistint_len(*h))
+		return (NULL);
+	if ((*h == NULL && idx > 0) || h == NULL)
 		return (NULL);
 	if (idx == 0 || *h == NULL)
 		new = add_dnodeint(h, n);
