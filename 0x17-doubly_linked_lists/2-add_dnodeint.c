019ff1fa4b9090f6b7c8c2eeafba8da38e0bc2d0
MichaelAuditore <michael_v613@hotmail.com>
2019-12-14 17:24:39 +0000

    [Fix] validating when head is not null

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -8,6 +8,7 @@
 dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
 	dlistint_t *new;
+	dlistint_t *tmp = *head;
 
 	new = malloc(sizeof(dlistint_t));
 	if (new == NULL)
@@ -15,6 +16,9 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 	new->n = n;
 	new->prev = NULL;
 	new->next = *head;
+
+	if (*head != NULL)
+	        tmp->prev = new;
 	*head = new;
-	return (new);
+	return (*head);
 }
