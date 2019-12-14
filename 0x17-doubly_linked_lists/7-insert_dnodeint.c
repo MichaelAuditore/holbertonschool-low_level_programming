249d792f7f4118bd4891631137fcc29a1abe8508
MichaelAuditore <michael_v613@hotmail.com>
2019-12-14 17:36:39 +0000

    [Fix] Adding the node in correct position

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -33,7 +33,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	new = malloc(sizeof(dlistint_t));
 	if (new == NULL)
 		return (NULL);
-	while (i <= idx)
+	while (i < idx - 1)
 	{
 		tmp = tmp->next;
 		i++;
