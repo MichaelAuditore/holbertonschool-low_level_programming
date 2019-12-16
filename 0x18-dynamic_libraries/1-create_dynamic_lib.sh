e2847247b7207d7dc7e5b9ac17dcae2b286ac368
MichaelAuditore <michael_v613@hotmail.com>
2019-12-16 17:01:19 +0000

    [Fix] name of library

--- 0x18-dynamic_libraries/1-create_dynamic_lib.sh
+++ 0x18-dynamic_libraries/1-create_dynamic_lib.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 gcc -fPIC -c *.c
-gcc -shared -Wl,-soname,libholberton.so -o libholberton.so *.o
+gcc -shared -Wl,-soname,liball.so -o liball.so *.o
 export LD_LIBRARY_PATH=.:$LD_LIBRARY_PATH
