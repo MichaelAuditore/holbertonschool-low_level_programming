1d320d09340429dc8aa9d14697564554e852cfab
MichaelAuditore <michael_v613@hotmail.com>
2019-12-16 22:00:16 +0000

    [Fix] Validating a library

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget /tmp/ https://github.com/MichaelAuditore/holbertonschool-low_level_programming/blob/master/0x18-dynamic_libraries/100-operations.so
-LD_PRELOAD = /tmp/:$LD_LIBRARY_PATH
+wget -qO /tmp/jackpot.so https://github.com/MichaelAuditore/holbertonschool-low_level_programming/blob/master/0x18-dynamic_libraries/100-operations.so
+export LD_PRELOAD=/tmp/jackpot.so
