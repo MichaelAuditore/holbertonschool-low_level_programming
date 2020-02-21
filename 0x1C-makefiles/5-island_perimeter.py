748c03badc5b72058f005e6cfde0ed711e501356
MichaelAuditore <michael_v613@hotmail.com>
2020-02-21 20:20:45 +0000

    [Fix] PEP8 Style and statements

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 """Island Perimeter Module"""
+
+
 def island_perimeter(grid):
     """
     Island Perimeter returns the perimeter of the island
@@ -11,7 +13,8 @@ def island_perimeter(grid):
     One cell is a square with side length 1
     Grid cells are connected horizontally/vertically (not diagonally).
     Grid is rectangular, width and height don’t exceed 100
-    Grid is completely surrounded by water, and there is one island (or nothing).
+    Grid is completely surrounded by water,
+    and there is one island (or nothing).
     The island doesn’t have “lakes”
     (water inside that isn’t connected to the water around the island).
     """
@@ -21,12 +24,13 @@ def island_perimeter(grid):
     """For loop to know how many landzone are in each row"""
     for col in range(cols):
         for row in range(rows):
-            if (grid[row][col] == 1 and grid[row + 1][col] != 1):
+            if (grid[row][col] == 1):
+                if (row == 0 or grid[row + 1][col] != 1):
                     perimeter += 1
-    """For loop to know how many landzone are in each col"""
-    for col in range(cols):
-        for row in range(rows):
-            if (grid[row][col] == 1 and grid[row][col + 1] != 1):
+                if (row == rows or grid[row - 1][col] != 1):
                     perimeter += 1
-
-    return (perimeter * 2)
+                if (col == 0 or grid[row][col + 1] != 1):
+                    perimeter += 1
+                if (col == cols or grid[row][col - 1] != 1):
+                    perimeter += 1
+    return (perimeter)
