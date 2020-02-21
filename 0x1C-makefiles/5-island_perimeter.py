3fd9b330b492c31e5407ef89a73007c082e3d00a
MichaelAuditore <michael_v613@hotmail.com>
2020-02-21 20:27:30 +0000

    [Fixing] Range of loop

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -18,15 +18,14 @@ def island_perimeter(grid):
     The island doesn’t have “lakes”
     (water inside that isn’t connected to the water around the island).
     """
-    rows = len(grid) - 1
     perimeter = 0
-    """For loop to know how many landzone are in each row"""
-    for row in range(rows):
+    """For loop to know how many landzone are in each row and col"""
+    for row in range(len(grid)):
         for col in range(len(grid[row])):
             if (grid[row][col] == 1):
                 if (row == 0 or grid[row - 1][col] == 0):
                     perimeter += 1
-                if (row == rows or grid[row + 1][col] == 0):
+                if (row == (len(grid) - 1) or grid[row + 1][col] == 0):
                     perimeter += 1
                 if (col == 0 or grid[row][col - 1] == 0):
                     perimeter += 1
