07b3c9477d6191a936cc309aa03f55b88327bf2d
MichaelAuditore <michael_v613@hotmail.com>
2020-04-15 11:27:08 -0500

    [Fix] Betty Style

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -1,5 +1,4 @@
 #include "search_algos.h"
-#include "stdio.h"
 /**
  * linear_search - searches for a value in an array of integers
  * using the linear search algorithm
@@ -12,12 +11,12 @@ int linear_search(int *array, size_t size, int value)
 {
 	size_t i = 0;
 
-	if(array == NULL || (int)size == 0)
+	if (array == NULL || (int)size == 0)
 		return (-1);
 
-	for (i=0; i < size; i++)
+	for (i = 0; i < size; i++)
 	{
-		printf("Value checked array[%d] = [%d]\n",(int)i, array[i]);
+		printf("Value checked array[%d] = [%d]\n", (int)i, array[i]);
 		if (value == array[i])
 			return (i);
 	}
